b11df38dd94e0f1889bfb0ab5ebe6460210beed8
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2021-04-16 17:10:36 +0600

    done react-slick

--- src/App.js
+++ src/App.js
@@ -3,45 +3,89 @@ import './App.css';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+const images = [
+  { src: "https://i.ibb.co/fHRRPCv/mainul01.png" },
+  { src: "https://i.ibb.co/MVzyt6t/mainul02.png" },
+  { src: "https://i.ibb.co/R9Ns7HS/mainul03.png"},
+  { src: "https://i.ibb.co/Sd0q6Fq/mainul04.png"},
+  { src: "https://i.ibb.co/k8zS9YM/mainul05.png" },
+  { src: "https://i.ibb.co/gdbpbST/mainul06.png"}
+];
+
 
 
 function App() {
   const settings = {
     dots: true,
-    dotsClass:'slick-dots slick-thumb',
+    dotsClassName:'slick-dots slick-thumb',
     className:'w-25 m-auto',
     infinite: true,
     arrows:true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    focusOnSelect:true
+    focusOnSelect:true, 
+    customPaging: function (i) {
+      return (
+        <a>
+          <img src={images[i].src} className="w-100 " style={{height:'40px'}} />
+        </a>
+      );
+    },
+
+     responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll:1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
     
 
   };
   return (
   <>
   <h1>Slider</h1>
+  
   <div className="bg-secondary">
         <h2> Single Item</h2>
         <Slider {...settings}>
           <div>
-            <h3>1</h3>
+          <img className="h-100 w-100" src="https://i.ibb.co/fHRRPCv/mainul01.png" />
           </div>
           <div>
-            <h3>2</h3>
+          <img className="h-100 w-100" src="https://i.ibb.co/MVzyt6t/mainul02.png" />
           </div>
           <div>
-            <h3>3</h3>
+          <img className="h-100 w-100" src="https://i.ibb.co/R9Ns7HS/mainul03.png" />
           </div>
           <div>
-            <h3>4</h3>
+          <img className="h-100 w-100" src="https://i.ibb.co/Sd0q6Fq/mainul04.png" />
           </div>
           <div>
-            <h3>5</h3>
+          <img className="h-100 w-100" src="https://i.ibb.co/k8zS9YM/mainul05.png" />
           </div>
           <div>
-            <h3>6</h3>
+          <img className="h-100 w-100" src="https://i.ibb.co/gdbpbST/mainul06.png" />
           </div>
         </Slider>
       </div>
